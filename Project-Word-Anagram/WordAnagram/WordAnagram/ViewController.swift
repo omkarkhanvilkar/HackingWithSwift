87150a1d0afe77c42327221dc831aa90546abc49
Omkar Khanvilkar <omkarkhanvilkar@Omkars-MacBook-Pro.local>
2019-04-04 01:09:32 -0400

    Added alert veiw to get answers

--- Project-Word-Anagram/WordAnagram/WordAnagram/ViewController.swift
+++ Project-Word-Anagram/WordAnagram/WordAnagram/ViewController.swift
@@ -15,16 +15,16 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startFileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startFileURL) {
                 allWords = startWords.components(separatedBy: "\n")
             }
         }
-        
         if allWords.isEmpty {
             allWords = ["Silkworm"]
         }
-        
         startGame()
     }
 
@@ -35,15 +35,55 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else {
+                return
+            }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String)  {
+        let lowerAns = answer.lowercased()
+        if isPossible(word: lowerAns) {
+            if isOriginal(word: lowerAns) {
+                if isReal(word: lowerAns) {
+                    usedWords.insert(answer, at: 0 )
+                    let indexpath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexpath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        return true
+    }
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
-        
         cell.textLabel?.text = usedWords[indexPath.row]
-        
         return cell
     }
 
